001553fc5965b91db8b2ec03c1ac40dcd36fd320
Luong Thanh Thangg <123573241+grayna0@users.noreply.github.com>
2024-04-02 22:01:39 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
-
-// vite.config.js
-export default {
   build: {
     rollupOptions: {
       output: {
@@ -17,4 +13,6 @@ export default {
       },
     },
   },
-};
+})
+
+
