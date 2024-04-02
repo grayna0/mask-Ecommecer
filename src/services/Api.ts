ef8b90a63992157d9a90702718824639f129fd90
Luong Thanh Thangg <123573241+grayna0@users.noreply.github.com>
2024-04-02 22:40:39 +0700

    Update Api.ts

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -3,7 +3,7 @@ import axios from "axios"
 
 export const api= axios.create(
    {
-     baseURL: "http://localhost:3001",
+     baseURL: "https://mask-z4vz.onrender.com",
   
      headers:{
         "Content-Type":"application/json",
