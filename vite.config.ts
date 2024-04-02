11a666978f6277bdae42c86eceb01eee6061377f
Luong Thanh Thangg <123573241+grayna0@users.noreply.github.com>
2024-04-02 21:59:31 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,3 +5,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+
+// vite.config.js
+export default {
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'chunk-name': ['module-name'],
+        },
+      },
+    },
+  },
+};
