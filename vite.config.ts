3fff8b06378828e240f43abcedc30a3bda01ca01
Luong Thanh Thangg <123573241+grayna0@users.noreply.github.com>
2024-04-02 22:26:55 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,13 @@
 // import react from '@vitejs/plugin-react'
 
 export default {
-build: {
-    chunkSizeWarningLimit: 500, // size là kích thước mới bạn muốn đặt, tính bằng kilobytes
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'chunk-name': ["@mui/x-data-grid","@emotion/react","@emotion/styled","@mui/material","@reduxjs/toolkit","material-ui-popup-state","react-hook-form","react-icons"],
+        },
+      },
+    },
   },
-}
+};
