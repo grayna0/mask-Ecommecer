a30628b7e3e98a048367ea29e52669547a858f79
Luong Thanh Thangg <123573241+grayna0@users.noreply.github.com>
2024-04-02 22:34:36 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,6 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default {
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'chunk-name': ["@mui/x-data-grid","@emotion/react","@emotion/styled","@mui/material","@reduxjs/toolkit","material-ui-popup-state","react-hook-form","react-icons"],
-        },
-      },
-    },
-  },
-};
+export default defineConfig({
+  plugins: [react()],
+})
