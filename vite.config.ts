68e9dda966fe5a695175d5e024224a2b3a484070
Luong Thanh Thangg <123573241+grayna0@users.noreply.github.com>
2024-04-02 22:17:37 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default {
 build: {
     chunkSizeWarningLimit: 1600, // size là kích thước mới bạn muốn đặt, tính bằng kilobytes
   },
-})
+}
