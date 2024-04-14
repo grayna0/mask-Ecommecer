840614671bd7290f4719f852ed85219d4f065b53
grayna0 <grayna0164@gmail.com>
2024-04-14 20:37:16 +0700

    update packagejson and db json

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -1,9 +1,9 @@
 import axios from "axios"
 
-
+// https://mask-z4vz.onrender.com
 export const api= axios.create(
    {
-     baseURL: "https://mask-z4vz.onrender.com",
+     baseURL: "http://localhost:3001",
   
      headers:{
         "Content-Type":"application/json",
